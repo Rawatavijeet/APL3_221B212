7f122a0d2026f7abf378883ceac4a8e27e212ef4
Kumar Avijeet <221b212@juetguna.in>
2025-03-28 14:14:21 +0530

    Delete LE5 directory

--- LE5/Main (12).java
+++
@@ -1,9 +0,0 @@
-public class Main{
-    public static void main(String args[]){
-        Beverage b = new Whisky();
-        b.templateMethod(30);
-        b = new Beer();
-        b.templateMethod(300);
-    }
-    
-}
--- LE5/Rum.java
+++
@@ -1,8 +0,0 @@
-class Rum extends Beverage {
-    protected void addContiment(){
-        System.out.println("Add chilled water");
-    }
-    protected void stir(){
-        System.out.println("Stir for 27 sec");
-    }
-}
